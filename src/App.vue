a9e3571d9373d4d3a504613341a4991744816355
stinface98 <austin.atmaja@gmail.com>
2018-03-04 14:36:09 -0500

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="esperto-navbar">
+      <div class="esperto-logo">
+        <img src="./assets/espertoLogo.png">
+      </div>
+      <div class="nav-logos">
+        <router-link to="Profile"><img src="./assets/profile-nav.png"></router-link>
+        <router-link to="StepCount"><img src="./assets/step-nav.png"></router-link>
+        <router-link to="HeartRate"><img src="./assets/heart-nav.png"></router-link>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -21,26 +30,30 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.esperto-navbar{
+  height: 60px;
+  border-bottom: solid 1px;
+  display: flex;
+  flex-direction: row;
+}
+.esperto-logo{
+  margin-left: 10px;
+  flex: 1;
+
+  img{
+    height: 50px;
+    vertical-align: middle;
+  }
+}
+.nav-logos{
+  img{
+    vertical-align: middle;
+    height: 35px;
+    margin-top: 12px;
+    margin-left: 3vw;
+    margin-right: 3vw;
+  }
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
